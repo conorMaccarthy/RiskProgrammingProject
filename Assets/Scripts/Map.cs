6f2b39cc18e0627d861cfd8c5a97bc4df18f4240
EthanHarder <117396275+EthanHarder@users.noreply.github.com>
2024-03-10 21:44:28 -0400

    New map, adjacency from .txt

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class Map:MonoBehaviour //for unity visuals, I need some functions.
 {
+    [SerializeField]
+    GameObject drawnLinePrefab;
+
     public List<Continent> continents = new List<Continent>(); //This value is initialized after all the continents have been initialized
 
 
@@ -28,8 +31,6 @@ public class Map:MonoBehaviour //for unity visuals, I need some functions.
                 {
                     drawPool.Add(GameObject.CreatePrimitive(PrimitiveType.Sphere));
                     var gO = drawPool[objectCount - 1];
-                    Debug.Log("Primitive Created");
-                    Debug.Log(gO.name);
                     gO.transform.position = new Vector2(ter.xOffset,ter.yOffset);
                     switch (ter.player)
                     {
@@ -58,12 +59,14 @@ public class Map:MonoBehaviour //for unity visuals, I need some functions.
     {
         StreamReader sR = new StreamReader("Assets/TerritoryFile.txt");
         string store= null;
+        Territory current;
+        Territory target;
         int indexContinent= -1;
         do
         {
-            Debug.Log("Next line");
+            current = null;
+            target = null;
             store = sR.ReadLine();
-            Debug.Log(store);
             if (store != null)
             {
                 string[] peices = store.Split(',');
@@ -74,13 +77,49 @@ public class Map:MonoBehaviour //for unity visuals, I need some functions.
                     Debug.Log(peices[1]);
                     continents.Add(new Continent(peices[0], int.Parse(peices[1])));
                 }
-                else if (peices.Length == 3)
+                else if (peices.Length >= 3)
                 {
                     continents[indexContinent].territories.Add(new Territory(peices[0], "Unowned", float.Parse(peices[1]), float.Parse(peices[2])));
+                    current = continents[indexContinent].territories[continents[indexContinent].territories.Count - 1];
+                    if (peices.Length > 3)
+                    {
+                        for (int i = 3; i < peices.Length; i++) //for every connection
+                        {
+                            target = FindTerritory(peices[i]);
+                            Debug.Log("ReadMap, Develop line from" + current.name + " to " + target.name);
+                            DrawLine(current, target);
+                            current.adjacentTerritoryList.Add(target);
+                            target.adjacentTerritoryList.Add(current);
+                        }
+                    }
+
                 }
             }
         } while (store != null);
         Debug.Log("Finished Reading File");
         sR.Close();
+    }
+
+    Territory FindTerritory(string targetName)
+    {
+        for (int contCount = 0; contCount < continents.Count; contCount++) //seach each continent
+        {
+            for (int terr = 0; terr < continents[contCount].territories.Count; terr++)
+            {
+                if (continents[contCount].territories[terr].name == targetName)
+                    return continents[contCount].territories[terr];
+            }
+        }
+
+        Debug.Log("That Territory is not found");
+        return null;
+    }
+
+    public void DrawLine(Territory start, Territory end)
+    {
+        GameObject newLine = Instantiate(drawnLinePrefab, Vector2.zero, Quaternion.identity);
+        newLine.GetComponent<LineRenderer>().SetPosition(0, new Vector2(start.xOffset, start.yOffset));
+        newLine.GetComponent<LineRenderer>().SetPosition(1, new Vector3(end.xOffset, end.yOffset,0));
+        
     }
 }
